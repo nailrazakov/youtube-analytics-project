9874989aef665c461fb01f7bcb96be8d314eb415
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2023-12-02 21:52:27 +0300

    correction to HomeWork4

--- src/video.py
+++ src/video.py
@@ -1,15 +1,20 @@
-from src.channel import Channel
+import os
+from googleapiclient.discovery import build
 
 
-class Video(Channel):
+class Video:
     """
     Класс для видео, который инициализируется 'id видео', название видео, ссылка на видео,
     количество просмотров, количество лайков
     """
+    # YOUT_API_KEY скопирован из гугла и вставлен в переменные окружения
+    API_KEY: str = os.getenv('YOUT_API_KEY')
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+
     def __init__(self, video_id: str):
-        youtube_object = super().youtube
-        video_response = youtube_object.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                           id=video_id).execute()
+        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                    id=video_id).execute()
         self.video_id: str = video_id
         self.video_title: str = video_response['items'][0]['snippet']['title']
         self.view_count: int = video_response['items'][0]['statistics']['viewCount']
@@ -17,12 +22,13 @@ class Video(Channel):
         self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
         self.url = "https://youtu.be/" + str(video_id)
 
-    def __str__(self): # Переопределяем
+    def __str__(self):  # Переопределяем
         return self.video_title
 
 
 class PLVideo(Video):
     """Класс для видео, который инициализируется 'id видео' и 'id плейлиста, наследуется из 'Video'"""
+
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
