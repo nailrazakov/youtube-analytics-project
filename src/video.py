be212878430eef208def53ffa1dc891789995c2b
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2023-12-08 20:43:32 +0300

    Homework6 включили обработку исключений

--- src/video.py
+++ src/video.py
@@ -13,17 +13,25 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, video_id: str):
-        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                    id=video_id).execute()
-        self.video_id: str = video_id
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
-        self.url = "https://youtu.be/" + str(video_id)
+        try:
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                        id=video_id).execute()
+            self.video_id: str = video_id
+            self.title: str = video_response['items'][0]['snippet']['title']
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+            self.url = "https://youtu.be/" + str(video_id)
+        except IndexError:  # у экземпляра инициализируется только свойство `video_id`, а остальные поля `None`.
+            self.video_id: str = video_id
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
+            self.url = None
 
     def __str__(self):  # Переопределяем
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
